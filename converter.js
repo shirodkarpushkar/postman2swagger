4d226580f44d041acd76c689916f30d0a273d6b4
shirodkarpushkar <pushkars@winjit.com>
2020-11-30 19:15:19 +0530

    rremoved unsed vars

--- converter.js
+++ converter.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const util = require("util");
-var swaggerJson = require("./input.json");
 const axios = require("axios");
 const apimaticURl = "https://www.apimatic.io";
 const prompt = require("prompt");
