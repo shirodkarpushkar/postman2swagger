abc12c58ad19a51410b7203407f11c4b1e53b030
shirodkarpushkar <pushkars@winjit.com>
2020-12-01 10:39:26 +0530

    deleted unnecessary files

--- test.js
+++
@@ -1,5 +0,0 @@
-var str =
-  'StreamChat error code 16: SendReaction failed with error: "message with id 714f7a13-aaee-4a39-bc7b-465de57cb76a not found"';
-
-
-var result = str.match(/"((?:\\.|[^"\\])*)"/);
